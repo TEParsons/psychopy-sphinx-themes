437a4023885df3e3f79707d1036d49f3b20ae984
TEParsons <todd.e.parsons@gmail.com>
2024-01-26 19:31:23 +0000

    ENH: Use abs paths

--- psychopy_sphinx_themes/__init__.py
+++ psychopy_sphinx_themes/__init__.py
@@ -6,6 +6,6 @@ def setup(app):
     # add_html_theme is new in Sphinx 1.6+
     if hasattr(app, 'add_html_theme'):
         # add psychopy core theme
-        app.add_html_theme('psychopy', str(Path(__file__).parent / 'psychopy'))
+        app.add_html_theme('psychopy', (Path(__file__).parent / 'psychopy').absolute())
         # add psychopy plugin theme
-        app.add_html_theme('psychopy_plugin', str(Path(__file__).parent / 'psychopy_plugin'))
+        app.add_html_theme('psychopy_plugin', (Path(__file__).parent / 'psychopy_plugin').absolute())
